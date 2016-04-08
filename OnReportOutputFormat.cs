7b5ffad714b9aa4de2cbdaf054f888169a0f77c2
Luiz Ricardo Abaque Coelho <luizracoelho@gmail.com>
2016-04-08 14:24:55 -0300

    Ordem do OnReportOutputFormat Alterada

--- OnReportOutputFormat.cs
+++ OnReportOutputFormat.cs
@@ -6,20 +6,20 @@
     public enum OnReportOutputFormat
     {
         /// <summary>
-        /// Output File: 'xls'
-        /// </summary>
-        EXCEL,
-        /// <summary>
-        /// Output File: 'tif'
-        /// </summary>
-        IMAGE,
-        /// <summary>
         /// Output File: 'pdf'
         /// </summary>
         PDF,
         /// <summary>
+        /// Output File: 'xls'
+        /// </summary>
+        EXCEL,
+        /// <summary>
+        /// <summary>
         /// Output File: 'doc'
         /// </summary>
-        WORD
+        WORD,
+        /// Output File: 'tif'
+        /// </summary>
+        IMAGE,
     }
 }
